ca6d0b5eaf3c9bddf5e1a034860a1a214e8ef713
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2018-12-08 14:49:20 +0300

    Release of is_prime()

--- hw_01/rsa.py
+++ hw_01/rsa.py
@@ -12,8 +12,11 @@ def is_prime(n):
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    for i in range(2, n):
+        if (n % i == 0):
+            return False
+
+    return True
 
 
 def gcd(a, b):
