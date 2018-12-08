bf4410c58b50f050d52558a3a3b1c2c4637169c6
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2018-12-08 14:25:56 +0300

    Release of encrypt_vigenere()

--- hw_01/vigenere.py
+++ hw_01/vigenere.py
@@ -9,7 +9,35 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    bigAlphabetStart = ord('A')
+    bigAlphabetEnd = ord('Z')
+    smallAlphabetStart = ord('a')
+    smallAlphabetEnd = ord('z')
+    ciphertext = ""
+
+    for i in range(len(plaintext)):
+        letterCode = ord(plaintext[i])
+        if bigAlphabetStart <= letterCode <= bigAlphabetEnd:
+            codesRange = [bigAlphabetStart, bigAlphabetEnd]
+        elif smallAlphabetStart <= letterCode <= smallAlphabetEnd:
+            codesRange = [smallAlphabetStart, smallAlphabetEnd]
+        else:
+            ciphertext += plaintext[i]
+            continue
+
+        keyLetterCode = ord(keyword[i % len(keyword)])
+        if bigAlphabetStart <= keyLetterCode <= bigAlphabetEnd:
+            shift = keyLetterCode - bigAlphabetStart
+        elif smallAlphabetStart <= keyLetterCode <= smallAlphabetEnd:
+            shift = keyLetterCode - smallAlphabetStart
+        else:
+            shift = 0
+
+        newLetterCode = letterCode + shift
+        if newLetterCode > codesRange[1]:
+            newLetterCode = codesRange[0] + (newLetterCode - codesRange[1] - 1)
+
+        ciphertext += chr(newLetterCode)
     return ciphertext
 
 
